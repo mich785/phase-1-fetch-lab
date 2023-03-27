a91188615d0560edb01ad2f4c9ecc7d876c926f6
michelle nderi <nderimichelle@gmail.com>
2023-03-27 13:51:22 +0300

    Updated assignment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
- fetch("https://anapioficeandfire.com/api/books")
+ return fetch("https://anapioficeandfire.com/api/books")
   .then((resp) => resp.json())
-  .then(data =>renderBooks(books))
+  .then(books =>renderBooks(books))
 }
 
 function renderBooks(books) {
